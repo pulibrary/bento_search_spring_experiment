995d79f425ffca2eed775cc218e37c704e6ac8a6
Jane Sandberg <js7389@princeton.edu>
2023-08-25 11:57:29 -0700

    Remove unneeded imports

--- src/test/java/edu/princeton/pulibrary/bento/catalog/CatalogControllerTests.java
+++ src/test/java/edu/princeton/pulibrary/bento/catalog/CatalogControllerTests.java
@@ -1,8 +1,6 @@
 package edu.princeton.pulibrary.bento.catalog;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -14,7 +12,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocumentList;
